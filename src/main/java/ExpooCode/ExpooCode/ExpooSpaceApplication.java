1e1910f65e9383a4d6c5f7a31e71d0d7c7bfafc5
Sebastian <sebasbteancourt14@gmail.com>
2025-09-10 21:11:13 -0500

    SpringBoot + Dependencias instalado

---
+++ src/main/java/ExpooCode/ExpooCode/ExpooSpaceApplication.java
@@ -0,0 +1,13 @@
+package ExpooCode.ExpooCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpooSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpooSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ExpooCode/ExpooCode/ExpooSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ExpooCode.ExpooCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpooSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
