a7d9a1468bf03a4984748c0d6c164133291fa57a
JXANX <castanedaloperaj@gmail.com>
2025-09-11 20:37:00 -0500

    Commit Repository´s

---
+++ src/main/java/ExpooCode/ExpooCode/persistence/repository/NotificacionRepository.java
@@ -0,0 +1,10 @@
+package ExpooCode.ExpooCode.persistence.repository;
+
+import ExpooCode.ExpooCode.persistence.entity.Notificacion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
+
+}
---
+++ src/main/java/ExpooCode/ExpooCode/persistence/repository/ReservaRepository.java
@@ -0,0 +1,10 @@
+package ExpooCode.ExpooCode.persistence.repository;
+
+import ExpooCode.ExpooCode.persistence.entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+}
